435bb9aa29d200dd4b1a04c355936aee78b2a2da
R a b i a <rabia.saeed.1995@gmail.com>
2022-07-28 16:41:25 -0400

    working on the footer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,21 @@ function Header() {
 	return (
 		<header>
 			<h1>Locally</h1>
+			<div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
+				<div className="inline-block">
+					<div className="inline-flex items-center max-w-full">
+						<button
+							className="flex items-center flex-grow-0 flex-shrink pl-2 relative w-60 border rounded-full px-1  py-1"
+							type="button"
+						>
+							<div className="block flex-grow flex-shrink overflow-hidden">
+								Where Search will go
+							</div>
+							<div className="flex items-center justify-center relative  h-8 w-8 rounded-full"></div>
+						</button>
+					</div>
+				</div>
+			</div>
 		</header>
 	);
 }
