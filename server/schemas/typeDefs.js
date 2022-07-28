5323c57690f3856e86042e96b9bd5d41088b93c9
BenBasic <benjaminmbasic@gmail.com>
2022-07-28 13:14:09 -0400

    Finalized typeDefs and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,12 +7,12 @@ const typeDefs = gql`
         username: String!
         email: String
         password: String
-        store: ID
+        store: Store
         reviews: [Reviews]
     }
 
     type Store {
-        storeId: ID!
+        _id: ID!
         products: [Products]
         rating: Int
         reviews: [Reviews]
@@ -20,8 +20,61 @@ const typeDefs = gql`
         email: String
         phoneNumber: String
         tags: [String]
+        createdAt: Date
     }
 
+    type Product {
+        _id: ID!
+        productTitle: String!
+        productDescription: String
+        ProductPrice: Int
+        ProductImage: String
+        stock: Int
+        likes: Int
+        tags: [String]
+
+    }
+
+    type Post {
+        _id: ID!
+        postContent: String
+        likes: Int
+        review: Boolean
+        createdAt: Date
+    }
+
+    input ProductInput {
+        productTitle: String!
+        productDescription: String
+        ProductPrice: Int
+        ProductImage: String
+        stock: Int
+        tags: [String]
+    }
+
+    input PostReviewInput {
+        postContent: String
+        review: Boolean
+        createdAt: Date
+    }
+
+    type Query {
+        me: User
+        getStores: [Store]
+        getProducts: [Product]
+        getPosts: [Post]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!, store: Store!): Auth
+        addProduct(productData: ProductInput!): Store
+        removeProduct(_id: ID!): Store
+        updateProduct(productData: ProductInput!): Store
+        addPostReview(postReviewData: PostReviewInput!): Store
+        removePostReview(_id: ID!): Store
+        updatePostReview(postReviewData: PostReviewInput!): Store
+    }
 `;
 
 // Exporting module so it can be used in other files
