e2e6bfeccb95eb47e0a4afc058d3596cb95c9e2a
DlonMusk <knight777@hotmail.ca>
2022-07-28 15:36:36 -0400

    got store functionality on add user working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,17 +2,21 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Store } = require("../models");
 const { signToken } = require("../utils/auth");
+const { ObjectId } = require('mongoose').Types;
 
 const resolvers = {
 
     Query: {
 
         me: async (parent, userData) => {
-
+            console.log(userData);
             if (userData) {
                 const user = await User
                     .findOne({ _id: userData._id })
                     .select("-__v -password")
+                    .populate('store')
+
+                    console.log(user);
 
                 return user;
             };
@@ -33,7 +37,11 @@ const resolvers = {
 
             // add store to user
             const store = await Store.create({email: userData.email})
-            user.store = store;
+            await user.update({store: store._id})
+
+            console.log(user);
+            console.log(user.store)
+            console.log(store);
 
             return user;
         },
