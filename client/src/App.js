bd4f29c951758c21655efeea35bd4a9ce27534a6
R a b i a <rabia.saeed.1995@gmail.com>
2022-07-28 16:22:22 -0400

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 // import Home from "./components/Home";
 // import Store from "./components/Store";
 // import Product from "./components/Product";
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Header />
 
 			<header className="App-header"></header>
-		</div>
+			<Footer />
+		</>
 	);
 }
 
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,1 +1,35 @@
 // Will add code here later
+import React from "react";
+
+import {
+	Box,
+	Container,
+	Stack,
+	Text,
+	Link,
+	useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Footer() {
+	return (
+		<Box
+			bg={useColorModeValue("gray.50", "gray.900")}
+			color={useColorModeValue("gray.700", "gray.200")}
+		>
+			<Container
+				as={Stack}
+				maxW={"6xl"}
+				py={4}
+				direction={{ base: "column", md: "row" }}
+				spacing={4}
+				justify={{ base: "center", md: "space-between" }}
+				align={{ base: "center", md: "center" }}
+			>
+				<Stack direction={"row"} spacing={6}>
+					<Link href={"#"}>Home</Link>
+				</Stack>
+				<Text>© 2022 Locally </Text>
+			</Container>
+		</Box>
+	);
+}
