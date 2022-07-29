d55ff756efac0cb67416efc674ee645aab3e8e34
BenBasic <benjaminmbasic@gmail.com>
2022-07-29 11:33:43 -0400

    Adding Apollo Client functionality to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import React from "react";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -12,6 +19,33 @@ import SignupForm from "./components/Auth/SignupForm";
 import AuthLayout from "./components/Auth/Layout";
 import ProfileLayout from "./components/Profile";
 import Signup from "./components/Auth/SignupForm";
+
+// Constructing an http link, assigning uri to the URL of the GraphQL endpoint to send requests to
+const httpLink = createHttpLink({
+	uri: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+	// Getting the authentication token from local storage
+	const token = localStorage.getItem("id_token");
+	// Returning the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+// Creating a new ApolloClient (This is an Apollo Client constructor)
+const client = new ApolloClient({
+	// Chaining the HTTP link and the authorization link
+	link: authLink.concat(httpLink),
+	// Assigning cache to InMemoryCache object, this stores the results of its GraphQL queries in cache
+	cache: new InMemoryCache(),
+});
+
+
 
 const Home = () => {
 	return (
@@ -32,7 +66,7 @@ const Store = () => {
 const Profile = () => {
 	return (
 		<>
-			<ProfileLayout />
+			<Profile />
 		</>
 	);
 };
@@ -59,17 +93,19 @@ const SignUp = () => {
 
 function App() {
 	return (
-		<>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="store" element={<Store />} />
-				<Route path="login" element={<Login />} />
-				<Route path="signup" element={<SignUp />} />
-				<Route path="profile" element={<Profile />} />
-			</Routes>
-			<Footer />
-		</>
+		<ApolloProvider client={client}>
+				<>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="store" element={<Store />} />
+						<Route path="login" element={<Login />} />
+						<Route path="signup" element={<SignUp />} />
+						<Route path="profile" element={<Profile />} />
+					</Routes>
+					<Footer />
+				</>
+		</ApolloProvider>
 	);
 }
 
