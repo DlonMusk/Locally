ed949aa786636a89873c58002ff8f80ae1f5e9af
R a b i a <rabia.saeed.1995@gmail.com>
2022-07-29 09:48:46 -0400

    footer placement

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,3 @@
-// Will add code here later
 import React from "react";
 
 import {
@@ -12,24 +11,26 @@ import {
 
 export default function Footer() {
 	return (
-		<Box
-			bg={useColorModeValue("gray.50", "gray.900")}
-			color={useColorModeValue("gray.700", "gray.200")}
-		>
-			<Container
-				as={Stack}
-				maxW={"6xl"}
-				py={4}
-				direction={{ base: "column", md: "row" }}
-				spacing={4}
-				justify={{ base: "center", md: "space-between" }}
-				align={{ base: "center", md: "center" }}
+		<div className="fixed bottom-0 left-0 right-0">
+			<Box
+				bg={useColorModeValue("gray.50", "gray.900")}
+				color={useColorModeValue("gray.700", "gray.200")}
 			>
-				<Stack direction={"row"} spacing={6}>
-					<Link href={"#"}>Home</Link>
-				</Stack>
-				<Text>© {new Date().getFullYear()} Locally </Text>
-			</Container>
-		</Box>
+				<Container
+					as={Stack}
+					maxW={"6xl"}
+					py={4}
+					direction={{ base: "column", md: "row" }}
+					spacing={4}
+					justify={{ base: "center", md: "space-between" }}
+					align={{ base: "center", md: "center" }}
+				>
+					<Stack direction={"row"} spacing={6}>
+						{/* <Link href={"#"}></Link> */}
+					</Stack>
+					<Text>© {new Date().getFullYear()} Locally </Text>
+				</Container>
+			</Box>
+		</div>
 	);
 }
