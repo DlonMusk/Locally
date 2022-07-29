34c36dc70ca8e9e56f9b8e7dd12a95ea81ea0337
R a b i a <rabia.saeed.1995@gmail.com>
2022-07-29 18:40:48 -0400

    added create store button and linked form

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -10,6 +10,7 @@ import { QUERY_GET_USER_STORE } from "../utils/queries";
 import Products from "./Products";
 import Posts from "./Posts";
 import Reviews from "./Reviews";
+import FormStore from "./FormStore";
 
 // Rabias stuff
 
@@ -31,7 +32,12 @@ const profile = {
 	],
 };
 
-export default function profileContainer() {
+export default function ProfileContainer() {
+	const [isShownStoreForm, setIsShownStoreForm] = useState(false);
+
+	const handleClick = (event) => {
+		setIsShownStoreForm(!isShownStoreForm);
+	};
 	////// Commented this bit out because Im attempting to work with Apollo Client
 
 	// const [userData, setUserData] = useState([
@@ -146,6 +152,9 @@ export default function profileContainer() {
 							<button
 								type="button"
 								className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+								onClick={() => {
+									handleClick();
+								}}
 							>
 								Create Store
 							</button>
@@ -157,6 +166,7 @@ export default function profileContainer() {
 						{profile.name}
 					</h1>
 				</div>
+				{isShownStoreForm && <FormStore />}
 			</div>
 
 			{/* <h2> Username: {userData[0].username}</h2>
