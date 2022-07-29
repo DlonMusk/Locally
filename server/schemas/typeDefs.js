bad8e16d048bf0e6c981ee165c1685444888ed9d
DlonMusk <knight777@hotmail.ca>
2022-07-28 20:06:04 -0400

    post mutations left to do

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -68,8 +68,8 @@ const typeDefs = gql`
 
     type Query {
         me(_id: ID!): User
-        getUserStore: Store
-        getUserProduct: Product
+        getUserStore(_id: ID): Store
+        getUserProduct(_id: ID): Product
         getStores: [Store]
         getProducts: [Product]
         getPosts: [Post]
@@ -79,8 +79,8 @@ const typeDefs = gql`
         login(_id: ID!, email: String!, password: String!): User
         addUser(username: String!, email: String!, password: String!): User
         addProduct(_id: ID!, productData: ProductInput!): User
-        removeProduct(_id: ID!): User
-        updateProduct(_id: ID!, productData: ProductInput!): User
+        removeProduct(productId: ID!, storeId: ID!): Store
+        updateProduct(productId: ID!, productData: ProductInput!): Product
         addPostReview(_id: ID!, postReviewData: PostReviewInput!): User
         removePostReview(_id: ID!): User
         updatePostReview(_id: ID!, postReviewData: PostReviewInput!): User
