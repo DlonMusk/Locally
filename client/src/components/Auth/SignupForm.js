effc4c203b6d469c38a438e78eb35da44a8ee275
BenBasic <benjaminmbasic@gmail.com>
2022-07-30 13:34:17 -0400

    Got create account functionality working, when signing up it will check for valid email and empty username/password fields, if the values of each field arent valid then the form will not proceed with token creation, if fields are valid then it will create a user in the database and assign a token. Next step is to now make password validation check with regex related helper, and make it so if the user is already logged in then they will be redirected to the home page instead of signup if they are somehow able to access the page while logged in

--- client/src/components/Auth/SignupForm.js
+++ client/src/components/Auth/SignupForm.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
+import { validateEmail } from "../../utils/helpers";
 
 
 const SignUp = () => {
 
 	// set initial form state
 	const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const { username, email, password } = userFormData;
+
+	/*
 	// set state for form validation
 	const [validated] = useState(false);
 	// set state for alert
 	const [showAlert, setShowAlert] = useState(false);
+	*/
 
 	/* Assigning an array containing the addUser resolver and an error object,
 	addUser will use the ADD_USER mutation, and error will log errors that occur
@@ -25,29 +33,51 @@ const SignUp = () => {
 
 	const handleFormSubmit = async (event) => {
 		event.preventDefault();
-	
-		// check if form has everything (as per react-bootstrap docs)
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-		  event.preventDefault();
-		  event.stopPropagation();
+
+		const validCheck = validateEmail(email);
+
+		if (!validCheck || username === '' || password === '') {
+            setErrorMessage("Please fill out a username, valid email, and password")
+        } else {
+			// If checks are passed, then the program will use emailJS and send the required information to be used to send an email to my inbox
+			console.log("Form data SUBMIT check", userFormData)
+		
+			/*
+			// check if form has everything (as per react-bootstrap docs)
+			const form = event.currentTarget;
+			if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+			}
+			console.log("VALID CHECK-------------")
+			console.log(form.checkValidity());
+			*/
+		
+			try {
+			const { data } = await addUser({
+				variables: { ...userFormData },
+			});
+			Auth.login(data.addUser.token);
+			} catch (err) {
+			console.error(err);
+			setErrorMessage("Something went wrong with the authentication process");
+			}
+		
+			setUserFormData({
+			username: '',
+			email: '',
+			password: '',
+			});
+			console.log("After submittion reset form data check", userFormData)
+
+			// Defining inputs to the query of the field ids for the form
+			const inputs = document.querySelectorAll('#username, #email, #password');
+
+			// Runs a for each method to clear the fields after hitting submit so that what was submitted doesnt stay in the form
+			inputs.forEach(input => {
+				input.value = '';
+			});
 		}
-	
-		try {
-		  const { data } = await addUser({
-			variables: { ...userFormData },
-		  });
-		  Auth.login(data.addUser.token);
-		} catch (err) {
-		  console.error(err);
-		  setShowAlert(true);
-		}
-	
-		setUserFormData({
-		  username: '',
-		  email: '',
-		  password: '',
-		});
 	};
 
 
@@ -56,7 +86,12 @@ const SignUp = () => {
 			<div className="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden">
 				<div className="px-4 py-8 sm:px-10">
 					<div className="mt-6">
-						<form noValidate validated={validated} onSubmit={handleFormSubmit} action="#" method="POST" className="space-y-6">
+						<form noValidate onSubmit={handleFormSubmit} action="#" method="POST" className="space-y-6">
+							{errorMessage && (
+							<div>
+								<p className="errorAlert">{errorMessage}</p>
+							</div>
+							)}
 							<div>
 								<label htmlFor="name" className="sr-only">
 									Username
@@ -68,7 +103,7 @@ const SignUp = () => {
 									autoComplete="username"
 									placeholder="Username"
 									onChange={handleInputChange}
-									value={userFormData.username}
+									value={username}
 									required
 									className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
 								/>
@@ -85,7 +120,7 @@ const SignUp = () => {
 									autoComplete="email"
 									placeholder="Email"
 									onChange={handleInputChange}
-									value={userFormData.email}
+									value={email}
 									required
 									className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
 								/>
@@ -102,7 +137,7 @@ const SignUp = () => {
 									placeholder="Password"
 									autoComplete="current-password"
 									onChange={handleInputChange}
-									value={userFormData.password}
+									value={password}
 									required
 									className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
 								/>
