df8738138e9a5df6b7ff13d7b7f9c7c5633c517f
R a b i a <rabia.saeed.1995@gmail.com>
2022-08-02 14:46:26 -0400

    product listing is now redirected to the right product/id page and you can add a review

--- client/src/components/ProductListing.js
+++ client/src/components/ProductListing.js
@@ -28,51 +28,50 @@ export default function ProductListing() {
 	`,
 	});
 
-	console.log("EEEEEEEEEE CONTEXT CHECK---------------")
-	console.log("")
+	console.log("EEEEEEEEEE CONTEXT CHECK---------------");
+	console.log("");
 
 	// product id
-	const testProductID = "62e5b958820df4975ed9ce44"
+	const testProductID = productId || "62e5b958820df4975ed9ce44";
 
-
-	const { loading, data, error } = useQuery(QUERY_GET_USER_PRODUCT, { variables: { id: testProductID }, });
+	const { loading, data, error } = useQuery(QUERY_GET_USER_PRODUCT, {
+		variables: { id: testProductID },
+	});
 
 	console.log("PRODUCT Data is---------------------");
-	console.log(data)
+	console.log(data);
 	console.log("PRODUCT loading is " + loading);
-	console.log("PRODUCT error is--------------------")
-	console.log(error)
-
-
+	console.log("PRODUCT error is--------------------");
+	console.log(error);
 
 	const productData = data?.getUserProduct || { "Didnt Get": "The Data" };
-	console.log("PRODUCT INFORMATION GRAB CHECK---------------")
+	console.log("PRODUCT INFORMATION GRAB CHECK---------------");
 	console.log(productData);
-	console.log(productData.reviews)
+	console.log(productData.reviews);
 
-	const productNestedReviews = productData.reviews
-	const productTags = productData.tags
-	console.log("NESTED REVIEWS4444444444444444444")
-	console.log(productNestedReviews)
-	console.log(typeof productNestedReviews)
+	const productNestedReviews = productData.reviews;
+	const productTags = productData.tags;
+	console.log("NESTED REVIEWS4444444444444444444");
+	console.log(productNestedReviews);
+	console.log(typeof productNestedReviews);
 
 	let reviewProductArray = [];
 	let tagArray = [];
 
 	for (var key in productNestedReviews) {
 		if (productNestedReviews.hasOwnProperty(key)) {
-			console.log("Made it here")
-			console.log(key)
-			console.log(productNestedReviews[key])
+			console.log("Made it here");
+			console.log(key);
+			console.log(productNestedReviews[key]);
 
-			const reviewNestedUserData = productNestedReviews[key].userData
-			console.log("THIS IS THE NESTED THING CHECK")
-			console.log(reviewNestedUserData)
+			const reviewNestedUserData = productNestedReviews[key].userData;
+			console.log("THIS IS THE NESTED THING CHECK");
+			console.log(reviewNestedUserData);
 			if (reviewNestedUserData !== null) {
-				console.log("222222222222222222222222222------------------------")
-				console.log(reviewNestedUserData.username)
-				console.log("keyyyyyyyyyyyyyyyyyyyyyyyyy")
-				console.log(key)
+				console.log("222222222222222222222222222------------------------");
+				console.log(reviewNestedUserData.username);
+				console.log("keyyyyyyyyyyyyyyyyyyyyyyyyy");
+				console.log(key);
 
 				reviewProductArray.push([
 					// INDEX 0 review id
@@ -89,14 +88,13 @@ export default function ProductListing() {
 					reviewNestedUserData._id,
 					// INDEX 6 username for user
 					reviewNestedUserData.username,
-				])
+				]);
 
-				console.log(typeof productTags)
-				console.log("P R O D U C T    P U S H     C H E C K --------")
-				console.log(reviewProductArray)
+				console.log(typeof productTags);
+				console.log("P R O D U C T    P U S H     C H E C K --------");
+				console.log(reviewProductArray);
 			}
-			console.log("MADE IT THERE")
-
+			console.log("MADE IT THERE");
 		}
 	}
 
@@ -104,15 +102,14 @@ export default function ProductListing() {
 		if (productTags.hasOwnProperty(tagIndex)) {
 			tagArray.push([
 				// INDEX 0 tag name
-				productTags[tagIndex]
-			])
+				productTags[tagIndex],
+			]);
 		}
 	}
 
-	console.log(tagArray)
-	console.log("THIS IS REVIEW ARRAYS")
-	console.log(reviewProductArray)
-
+	console.log(tagArray);
+	console.log("THIS IS REVIEW ARRAYS");
+	console.log(reviewProductArray);
 
 	return (
 		<div className="bg-white">
@@ -142,7 +139,9 @@ export default function ProductListing() {
 
 						<div className="mt-3">
 							<h2 className="sr-only">Product information</h2>
-							<p className="text-3xl text-gray-900">${productData.productPrice}</p>
+							<p className="text-3xl text-gray-900">
+								${productData.productPrice}
+							</p>
 						</div>
 
 						<div className="mt-6">
@@ -150,11 +149,11 @@ export default function ProductListing() {
 
 							<div
 								className="text-base text-gray-700 space-y-6"
-								dangerouslySetInnerHTML={{ __html: productData.productDescription }}
+								dangerouslySetInnerHTML={{
+									__html: productData.productDescription,
+								}}
 							/>
 						</div>
-
-						
 
 						<ul role="list" className="divide-y divide-gray-200">
 							{reviewProductArray.map((item) => (
@@ -167,19 +166,13 @@ export default function ProductListing() {
 										/>
 										<div className="flex-1 space-y-1">
 											<div className="flex items-center justify-between">
-												<h3 className="text-sm font-semibold">
-													{item[6]}
-												</h3>
+												<h3 className="text-sm font-semibold">{item[6]}</h3>
 												<p className="text-sm font-medium text-gray-700">
 													{productData.productTitle}
 												</p>
-												<p className="text-sm text-gray-500">
-													{item[4]}
-												</p>
+												<p className="text-sm text-gray-500">{item[4]}</p>
 											</div>
-											<p className="text-sm text-gray-500">
-												{item[1]}
-											</p>
+											<p className="text-sm text-gray-500">{item[1]}</p>
 										</div>
 									</div>
 								</li>
@@ -199,7 +192,7 @@ export default function ProductListing() {
 									type="button"
 									className="ml-4 py-3 px-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500"
 								>
-									<Like productId={productId} />
+									<Like componentId={productId} />
 									<span className="sr-only">Add to favorites</span>
 								</button>
 							</div>
@@ -207,7 +200,7 @@ export default function ProductListing() {
 						<button
 							type="button"
 							onClick={() => setShowReviewForm(!showReviewForm)}
-							className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							className=" mt-6 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 						>
 							Write Review
 						</button>
