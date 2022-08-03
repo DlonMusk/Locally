0defc69e26e57ebdd8997fb99d56c642e3dc3368
DlonMusk <knight777@hotmail.ca>
2022-08-03 01:35:45 -0400

    testing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -46,10 +46,6 @@ export default function Header(props) {
 			name: "Home",
 			path: "/",
 		},
-		{
-			name: "Store",
-			path: "/store",
-		},
 	];
 
 	const activeNavStyle =
@@ -168,19 +164,6 @@ export default function Header(props) {
 															)}
 														>
 															Your Profile
-														</a>
-													)}
-												</Menu.Item>
-												<Menu.Item>
-													{({ active }) => (
-														<a
-															href="#"
-															className={classNames(
-																active ? "bg-gray-100" : "",
-																"block px-4 py-2 text-sm text-gray-700"
-															)}
-														>
-															Settings
 														</a>
 													)}
 												</Menu.Item>
