e98b75ce167f72bee0f498f2fe451d863a177ca9
DlonMusk <knight777@hotmail.ca>
2022-08-03 00:38:40 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -22,9 +22,11 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static('client/build'));
 }
-
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
