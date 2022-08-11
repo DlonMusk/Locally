09619d0e9fe665d346fa0bb656d21ec7de9c6e9b
DlonMusk <knight777@hotmail.ca>
2022-08-11 13:00:07 -0400

    remove store resolver added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -198,7 +198,7 @@ export default function Header() {
 							{/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
 							<Disclosure.Button
 								as="a"
-								href="#"
+								href="/"
 								className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
 							>
 								Home
@@ -220,7 +220,7 @@ export default function Header() {
 								</div>
 								<div className="ml-3">
 									<div className="text-base font-medium text-gray-800">
-										tom
+										
 									</div>
 									<div className="text-sm font-medium text-gray-500">
 										tom@example.com
